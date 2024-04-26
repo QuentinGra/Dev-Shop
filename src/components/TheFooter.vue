d9befba20daab176b0173adb0247165684689640
QuentinGra <pilou4260042@gmail.com>
2024-04-26 15:19:30 +0200

    feat(home): add footer

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,7 +1,45 @@
 <script setup lang="ts"></script>
 
 <template>
-  <footer></footer>
+  <footer>
+    <div class="container text-center d-md-flex justify-content-between py-4 mt-4 border-top">
+      <div class="mb-2 mb-md-0">
+        <ul class="p-0 m-0">
+          <li class="d-inline-block me-2">
+            <a href="" target="_blank" class="text-muted">
+              <i class="bi bi-facebook"></i>
+            </a>
+          </li>
+          <li class="d-inline-block me-2">
+            <a href="" target="_blank" class="text-muted">
+              <i class="bi bi-github"></i>
+            </a>
+          </li>
+          <li class="d-inline-block me-2">
+            <a href="" target="_blank" class="text-muted">
+              <i class="bi bi-twitter-x"></i>
+            </a>
+          </li>
+          <li class="d-inline-block">
+            <a href="" target="_blank" class="text-muted">
+              <i class="bi bi-linkedin"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div class="text-center">
+        <p>
+          © 2024 Copyright
+          <a
+            href="https://github.com/QuentinGra/Dev-Shop"
+            class="fw-bold text-warning"
+            target="_blank"
+            >Flashy Coder</a
+          >
+        </p>
+      </div>
+    </div>
+  </footer>
 </template>
 
 <style scoped></style>
