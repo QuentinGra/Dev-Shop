3e7630f76c8915f9249199ad81260190e0ab304b
QuentinGra <pilou4260042@gmail.com>
2024-04-26 14:49:08 +0200

    feat(home): add main

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
 import TheHeader from '@/components/TheHeader.vue'
-import TheMain from '@/components/TheMain.vue'
+import TheHome from '@/views/TheHome.vue'
 import TheFooter from '@/components/TheFooter.vue'
 </script>
 
 <template>
   <TheHeader />
-  <TheMain />
+  <TheHome />
   <TheFooter />
 </template>
 
---
+++ src/views/TheHome.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import ProductList from '@/components/ProductList.vue'
+</script>
+
+<template>
+  <main class="mt-5">
+    <div class="d-md-flex">
+      <div class="flex-fill">
+        <ProductList />
+      </div>
+    </div>
+  </main>
+</template>
+
+<style scoped></style>
