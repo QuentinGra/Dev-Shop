0eabe91817d53c5a7e055e87d5352a7f81e8231d
QuentinGra <pilou4260042@gmail.com>
2024-04-26 09:31:47 +0200

    feat(home): adding theme in header

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -3,12 +3,15 @@
 <template>
   <header class="navbar navbar-expand-lg fixed-top bg-body-tertiary">
     <div class="container-fluid text-uppercase">
-      <button class="navbar-toggler collapsed" data-bs-target="#navbar">
+      <button class="navbar-toggler collapsed" data-bs-toggle="collapse" data-bs-target="#navbar">
         <span class="navbar-toggler-icon"></span>
       </button>
       <a href="/" class="navbar-brand">
         <img src="/img/logo.svg" alt="LOGO DS" width="32" />
       </a>
+      <a href="/" class="position-relative me-3 d-lg-none"
+        ><i class="bi bi-cart-check-fill fs-4"></i
+      ></a>
       <nav id="navbar" class="collapse navbar-collapse">
         <ul class="navbar-nav me-auto">
           <li class="nav-item"><a href="/" class="nav-link">Accueil</a></li>
@@ -25,7 +28,9 @@
             <a href="/" class="nav-link">Connexion<i class="bi bi-box-arrow-in-right"></i></a>
           </li>
           <li class="nav-item dropdown">
-            <button class="nav-link dropdown-toggle text-uppercase">Thèmes</button>
+            <button class="nav-link dropdown-toggle text-uppercase" data-bs-toggle="dropdown">
+              Thèmes
+            </button>
             <ul class="dropdown-menu">
               <li><button class="dropdown-item">Clair</button></li>
               <li><button class="dropdown-item">Sombre</button></li>
